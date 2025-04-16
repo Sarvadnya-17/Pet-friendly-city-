6a09e42798245273d63b041afae47c39a3925ce8
Sarvadnya-17 <sarvadnyarisbudiesf@gmail.com>
2025-04-16 12:56:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-4xl font-bold text-orange-600 mb-4">Welcome to Pet-Friendly City!</h1>
+      <p className="text-lg">A community built for pets and their people.</p>
+    </div>
+  );
+}
+
+export default App;
